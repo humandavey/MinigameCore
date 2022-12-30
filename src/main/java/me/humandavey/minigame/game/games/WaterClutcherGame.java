bdaf9c67d0f348d1398b1c031363eb9d9af4b588
humandavey <davey.adams.three@gmail.com>
2022-12-30 15:10:15 -0600

    Initial Commit

--- src/main/java/me/humandavey/minigame/game/games/WaterClutcherGame.java
+++ src/main/java/me/humandavey/minigame/game/games/WaterClutcherGame.java
@@ -2,6 +2,9 @@ package me.humandavey.minigame.game.games;
 
 import me.humandavey.minigame.game.Game;
 import me.humandavey.minigame.instance.Arena;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class WaterClutcherGame extends Game {
 
@@ -11,6 +14,8 @@ public class WaterClutcherGame extends Game {
 
 	@Override
 	public void onStart() {
-
+		for (Player player : arena.getAlivePlayers()) {
+			player.getInventory().addItem(new ItemStack(Material.WATER_BUCKET));
+		}
 	}
 }
